af72c16ab243ce00964725f3b4274df43151244b
abdulhafiz0007 <150119146+abdulhafiz0007@users.noreply.github.com>
2024-01-23 13:23:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+// TASK 1 
+
+// var elForm = document.querySelector(".js-form")
+// var elInput = elForm.querySelector(".js-input")
+// var elResult = document.querySelector(".js-result")
+
+// var lastLetterCount = 0
+
+// elForm.addEventListener("submit", function(evt) {
+    
+//     evt.preventDefault();
+    
+//     inputVal = elInput.value;
+    
+//     var inputValLastLetter = inputVal.slice(-1);
+    
+//     for( var i = 0; i < inputVal.length; i++) {
+//         if(inputVal[i] === inputValLastLetter) {
+//             lastLetterCount++
+//         }
+        
+//     }
+
+//     elResult.textContent = lastLetterCount;
+    
+// })
+
+
+
+// TASK 2 
+
+// O'rta arfimetigini topish algorithmi -->
+
+//  var numbers = [1, 2, 3, 4]
+
+//  var sumValue = 0
+
+//  for( var number of numbers) {
+//     sumValue += number;
+// }
+
+// var quanitityOfNumbers = numbers.length;
+// var AverageMeanValue = sumValue / quanitityOfNumbers;
+
+// console.log(`Bu raqamlarning O'rta Arifmetigi: ${AverageMeanValue} ga teng`)
+
+
+
+// TASK 3
+
+// Arraydagi birinchi elementni boshidagi va oxiridagi harflarini katta harfda qilib berish -->
+
+// var stringArray = ["olma", "uzum", "anor"]
+
+// var firstElArray = stringArray[0];
+// var firstElArrayFirstIndex = firstElArray[0].toUpperCase()
+// var firstElArrayLastIndex = firstElArray.charAt(firstElArray.length-1).toUpperCase()
+// var result = firstElArrayFirstIndex + firstElArray.substring(1, firstElArray.length-1) + firstElArrayLastIndex;
+
+// console.log(result);
+
+
+
+
